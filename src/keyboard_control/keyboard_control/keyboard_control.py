f2f1e0da58756b456125d7944997e470a59c811d
dongdongO <tjehdghks4356@gmail.com>
2024-06-03 23:28:47 +0200

    update test case

--- src/keyboard_control/keyboard_control/keyboard_control.py
+++ src/keyboard_control/keyboard_control/keyboard_control.py
@@ -50,7 +50,7 @@ class KeyboardControl(Node):
     def publish_steer_control(self, steer_control):
         steer_control_msg = Float32()
         steer_control_msg.data = steer_control
-        self.steer_control_publisher_.publish(steer_control_msg)
+        # self.steer_control_publisher_.publish(steer_control_msg)
         
     def publish_velocity_control(self, velocity_control):
         velocity_control_msg = Float32()
