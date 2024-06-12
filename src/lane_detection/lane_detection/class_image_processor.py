882bf8b21784b766cb03cb2914e8b9fc5e2ad479
mczb <12190588@inha.edu>
2024-06-12 12:45:29 +0200

    Remove calculate_relative_path

--- src/lane_detection/lane_detection/class_image_processor.py
+++ src/lane_detection/lane_detection/class_image_processor.py
@@ -216,29 +216,3 @@ class ImageProcessor():
                 middle[i] = (x_right[0]-self.half_lane_width,x_right[1])
         return middle
     
-    def calculate_relative_path(self, middle, height, width):
-        relative_middle = []
-        for i in range(len(middle) - 1):
-            if middle[i] is not None and middle[i + 1] is not None:
-                x1, y1 = middle[i]
-                x2, y2 = middle[i + 1]
-                if x1 < 0 or y1 < 0 or x2 < 0 or y2 < 0:
-                    continue
-                relative_x1 = width - x1
-                relative_y1 = height - y1
-                relative_x2 = width - x2
-                relative_y2 = height - y2
-
-                yaw = math.atan2(relative_y2 - relative_y1, relative_x2 - relative_x1)
-                relative_middle.append((relative_x1, relative_y1, yaw))
-        
-        # Add the last point with the same yaw as the previous one
-        if middle[-1] is not None and relative_middle:
-            x, y = middle[-1]
-            if x >= 0 and y >= 0:
-                relative_x = width - x
-                relative_y = height - y
-                _, _, last_yaw = relative_middle[-1]
-                relative_middle.append((relative_x, relative_y, last_yaw))
-        
-        return relative_middle
